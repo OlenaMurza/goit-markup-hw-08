a912a3e766788079e63cec21662ea6c2b7d11430
OlenaMurza <murzaelena@gmail.com>
2022-10-02 18:47:09 +0300

    update

---
+++ js/_mobile-menu.js
@@ -0,0 +1,12 @@
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document / querySelector("[data-menu]");
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded =
+    menuBtnRef.getAttribute("aria-extended") === "true" |  false;
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("artia-expanded", !expanded);
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})();
